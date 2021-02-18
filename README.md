7230ce83237df3e420d59228ae26d97320a76bfe
Lochlann O'Regan <27902576+lochlannoregan@users.noreply.github.com>
2021-02-18 00:54:42 +0000

    Initial Model Development Work
    Testing different steps understand and clean bank classification dataset ready for model training.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Testing SHAP Algorithms
+
+
+# Datasets
+- Bank
+    - Classification
+    - https://archive.ics.uci.edu/ml/datasets/Bank+Marketing#
+    - Requires citation - [Moro et al., 2014] S. Moro, P. Cortez and P. Rita. A Data-Driven Approach to Predict the Success of Bank 
+    Telemarketing. Decision Support Systems, Elsevier, 62:22-31, June 2014
+- 
---
+++ bank_classification.py
@@ -0,0 +1,59 @@
+# !/usr/bin/env python3
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+
+
+def import_data():
+    return pd.read_csv("data/bank/bank.csv", sep=";")
+
+
+def training_testing_split(data):
+    X_train, X_test, y_train, y_test = train_test_split(data.drop(['y'], axis=1), data['y'])
+    return X_train, X_test, y_train, y_test
+
+
+# Data pre-processing / cleaning etc. - data preparation is right name??
+def pre_processing(data):
+    # data = pd.get_dummies(data, columns=['job', 'marital', 'education', 'default', 'housing',
+    #                                      'loan', 'contact', 'month', 'poutcome', 'y'])
+    # data['Test'] = pd.factorize(data.job)[0]
+    # print(data.job.value_counts())
+    # print(data.Test.value_counts())
+
+    le = LabelEncoder()
+
+    print(data.job.value_counts())
+    data['job_encoded'] = le.fit_transform(data.job)
+    data['marital_encoded'] = le.fit_transform(data.marital)
+    data['education_encoded'] = le.fit_transform(data.education)
+
+    print("finished")
+    return data
+
+
+def main():
+    data = import_data()
+    data = pre_processing(data)
+
+    print(data.head())
+
+    # X_train, X_test, y_train, y_test = training_testing_split(data)
+
+
+
+    # X_train, X_test, y_train, y_test = pre_processing(X_train, X_test, y_train, y_test)
+    # linear_clf = linear_classification_training(X_train, y_train)
+    # decision_tree_clf = decision_tree(X_train, y_train)
+    # print(accuracy_score(y_test, linear_clf.predict(X_test)))
+
+
+if __name__ == '__main__':
+    main()
