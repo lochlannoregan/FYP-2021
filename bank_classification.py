3969e44ce2d88585b9997c21f22d0b962e9a5305
Lochlann O'Regan <27902576+lochlannoregan@users.noreply.github.com>
2021-03-01 10:53:28 +0000

    Initial pre-processing step with imbalanced-learn
    Making use of Synthetic Minory Oversampling Technique (SMOTE) from imbalanced-learn to account for the minority class where clients subscribe to a term deposit product. This accounts for ~12% of the dataset.
    The bank-additional dataset files have been removed as the increased number of features and presence of missing values are not desired for this usecase.

--- bank_classification.py
+++ bank_classification.py
@@ -4,10 +4,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, precision_recall_curve, \
-    plot_precision_recall_curve, average_precision_score
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
 import shap
 import matplotlib.pyplot as plt
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.pipeline import make_pipeline
+import scipy as sc
+from imblearn.over_sampling import SMOTE
+from collections import Counter
 
 
 def import_data():
@@ -16,11 +20,15 @@ def import_data():
 
 def pre_processing(data):
     X_train, X_test, y_train, y_test = train_test_split(data.drop(['y_encoded'], axis=1), data['y_encoded'])
-    scaler = StandardScaler()
-    scaler.fit(X_train)
-    scaler.transform(X_train)
-    scaler.transform(X_test)
-    return X_train, X_test, y_train, y_test
+    print(Counter(y_train).items())
+    X_train_resampled, y_train_resampled = SMOTE().fit_resample(X_train, y_train)
+    print(Counter(y_train).items())
+    # scaler = StandardScaler()
+    # scaler.fit(X_train)
+    # scaler.transform(X_train)
+    # scaler.transform(X_test)
+
+    return X_train_resampled, X_test, y_train_resampled, y_test
 
 
 # Data pre-processing / cleaning etc. - data preparation is right name??
@@ -48,6 +56,8 @@ def data_preparation(data):
                          'housing_encoded', 'loan_encoded', 'contact_encoded', 'day', 'month_encoded',
                          'duration', 'campaign', 'pdays', 'previous', 'poutcome_encoded', 'y_encoded']].copy()
 
+    # data_numeric = data[['age', 'default_encoded', 'balance', 'housing_encoded', 'loan_encoded', 'duration', 'campaign', 'pdays', 'previous', 'poutcome_encoded', 'y_encoded']].copy()
+
     return data_numeric
 
 
@@ -58,21 +68,21 @@ def mlp_model(X_train, y_train, X_test, y_test):
     # X_train = sc.fit_transform(X_train)
     # X_test = sc.transform(X_test)
 
+    # pipeline = make_pipeline(StandardScaler(), MLPClassifier(hidden_layer_sizes=8))
+    # #
+    # # Create the randomized search estimator
+    # #
+    # param_distributions = [{'mlp__C': sc.stats.expon(scale=100)}]
+    # rs = RandomizedSearchCV(estimator=pipeline, param_distributions=param_distributions,
+    #                         cv=10, scoring='accuracy', refit=True, n_jobs=1,
+    #                         random_state=1)
+    #
+    # rs.fit(X_train, y_train)
+
     # sklearn neural network
     # mlpc = MLPClassifier(learning_rate_init=0.07, activation='logistic', hidden_layer_sizes=5, max_iter=500)
-    mlpc = MLPClassifier()
+    mlpc = MLPClassifier(hidden_layer_sizes=[8])
     mlpc.fit(X_train, y_train)
-
-    # accuracy = str(accuracy_score(y_test, pred_mlpc) * 100)
-
-    # # print the models performance
-    # print("\t\tAccuracy: " + accuracy)
-    # print("Accuracy: " + accuracy + "\n")
-    # print(str(classification_report(y_test, pred_mlpc)) + "\n")
-    # print(str(confusion_matrix(y_test, pred_mlpc)) + "\n\n")
-    # output_file.write("Accuracy: " + accuracy + "\n")
-    # output_file.write(str(classification_report(y_test, pred_mlpc)) + "\n")
-    # output_file.write(str(confusion_matrix(y_test, pred_mlpc)) + "\n\n")
 
     return mlpc
 
@@ -80,7 +90,17 @@ def mlp_model(X_train, y_train, X_test, y_test):
 def model_performance(mlpc, X_test, y_test):
     pred_mlpc = mlpc.predict(X_test)
     print(str(classification_report(y_test, pred_mlpc)) + "\n")
-    print(str(confusion_matrix(y_test, pred_mlpc)) + "\n\n")
+    print(str(confusion_matrix( y_test, pred_mlpc)) + "\n\n")
+
+    fpr, tpr, thresholds = roc_curve(y_test[:-1], pred_mlpc[:-1])
+    auc_mlp = auc(fpr, tpr)
+
+    plt.figure(figsize=(5, 5), dpi=100)
+    plt.plot(fpr, tpr, linestyle='-', label='MLP (auc = %0.3f)' % auc_mlp)
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.legend()
+    plt.show()
 
 
 def shap_usage(X_train, X_test, mlpc):
@@ -98,17 +118,13 @@ def main():
     data = import_data()
     data_numeric = data_preparation(data)
 
-    print(data_numeric.head())
-
     X_train, X_test, y_train, y_test = pre_processing(data_numeric)
 
     mlpc = mlp_model(X_train, y_train, X_test, y_test)
 
     model_performance(mlpc, X_test, y_test)
 
-    shap_usage(X_train, X_test, mlpc)
-
-    print("Testing line")
+    # shap_usage(X_train, X_test, mlpc)
 
 
 if __name__ == '__main__':
