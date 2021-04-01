f4d102c14eb64c7dbbc76d61ede8da8e86d3c1fc
Lochlann O'Regan <27902576+lochlannoregan@users.noreply.github.com>
2021-04-01 21:06:44 +0100

    Snippets added to successfully generate various SHAP graphics

--- bank-marketing-additional.py
+++ bank-marketing-additional.py
@@ -14,6 +14,7 @@ from sklearn.impute import SimpleImputer
 import pickle
 import joblib
 
+
 def train_save_model():
     data = pd.read_csv("data/bank-additional/bank-additional.csv", sep=";")
 
@@ -123,29 +124,54 @@ def train_save_model():
     joblib.dump(y_test, "y_test.joblib")
     joblib.dump(y_train, "y_train.joblib")
 
+    return mlp_classifier, X_test, X_train, y_test, y_train
 
-def load_shap_model():
+
+def load_model():
     mlp_classifier = joblib.load("mlp_classifier.joblib")
     X_test = joblib.load("x_test.joblib")
     X_train = joblib.load("X_train.joblib")
     y_test = joblib.load("y_test.joblib")
     y_train = joblib.load("y_train.joblib")
 
-    import shap
-    shap.initjs()
-    explainer = shap.KernelExplainer(mlp_classifier.predict_proba, X_train)
-    shap_values = explainer.shap_values(X_test.iloc[0])
-    shap.force_plot(explainer.expected_value[0], shap_values[0], X_test.iloc[0], matplotlib=True)
+    return mlp_classifier, X_test, X_train, y_test, y_train
+
+
+def shap(mlp_classifier, X_test, X_train, y_test, y_train):
+    #
+    # import shap
+    # shap.initjs()
+    # explainer = shap.KernelExplainer(mlp_classifier.predict_proba, X_train)
+    # shap_values = explainer.shap_values(X_test.iloc[0], nsamples=1000)
+    # shap.force_plot(explainer.expected_value[0], shap_values[0], X_test.iloc[0], matplotlib=True)
 
     # import shap
     # shap.initjs()
-    # shap_explainer = shap.KernelExplainer(mlp_classifier.predict(), X_test.iloc[0])
-    # shap_value = shap_explainer.explain()
-    # shap.plots.waterfall(shap_value[0])
+    # f = lambda x: mlp_classifier.predict_proba(x)[:, 1]
+    # med = X_train.median().values.reshape((1, X_train.shape[1]))
+    # explainer = shap.Explainer(f, med)
+    # shap_values = explainer(X_test.iloc[0:1000, :])
+    # shap.plots.waterfall(shap_values[0])
+
+    # import shap
+    # shap.initjs()
+    # X_train_summary = shap.kmeans(X_train, 10)
+    # explainer = shap.KernelExplainer(mlp_classifier.predict, X_train_summary)
+    # shap_values = explainer.shap_values(X_test)
+    # shap.summary_plot(shap_values, X_test)
+
+    import shap
+    shap.initjs()
+    X_train_summary = shap.kmeans(X_train, 10)
+    explainer = shap.KernelExplainer(mlp_classifier.predict, X_train_summary)
+    shap_values = explainer.shap_values(X_test)
+    shap.dependence_plot("duration", shap_values, X_test)
 
 
 def main():
-    load_shap_model()
+    # mlp_classifier, X_test, X_train, y_test, y_train = train_save_model()
+    mlp_classifier, X_test, X_train, y_test, y_train = load_model()
+    shap(mlp_classifier, X_test, X_train, y_test, y_train)
 
 
 if __name__ == "__main__":
